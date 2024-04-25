1381120a86b41704b121cd795ec17d5c5a2150e8
imanolazpiroz17 <imanolazpiroz77@gmail.com>
2024-04-25 16:34:23 -0300

    Practica 2 Update

--- Modulo Objetos/Practica/Practica1/P2E2.java
+++ Modulo Objetos/Practica/Practica1/P2E2.java
@@ -1,9 +1,17 @@
 package tema1;
 
+/*
+2- Escriba un programa que lea las alturas de los 15 jugadores de un equipo de
+básquet y las almacene en un vector. Luego informe:
+- la altura promedio
+- la cantidad de jugadores con altura por encima del promedio
+NOTA: Dispone de un esqueleto para este programa en Ej02Jugadores.java
 
 //Paso 1: Importar la funcionalidad para lectura de datos
 import PaqueteLectura.Lector;
 import PaqueteLectura.GeneradorAleatorio;
+*/
+
 
 public class Ej02Jugadores {
 
